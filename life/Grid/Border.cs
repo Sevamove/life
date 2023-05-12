0830506446181c9a88c27aa1f3e4d50819e48b03
Vsevolod <hello@vsevolod.nl>
2023-05-12 21:10:42 +0200

    fix: added code improvements

--- life/Grid/Border.cs
+++ life/Grid/Border.cs
@@ -1,49 +1,49 @@
-namespace Life
-{
-	interface IBorder
-	{
-		// Read methods.
 
-		public bool IsAt(Position position);
+using Life.CellPosition;
+using Life.GridArea;
+using Life.GridSideBorders;
+
+namespace Life.GridBorder;
+
+interface IBorder
+{
+	// Read methods.
+
+	public bool Is(Position position);
+	public bool Is(List<ISideBorder> sideBorder, Position position);
+}
+
+public class Border : IBorder
+{
+	private readonly Area area;
+
+	public Border(Area area)
+	{
+		this.area = area;
 	}
 
-	public class Border : IBorder
+	public bool Is(Position position)
 	{
-		private readonly Area area;
+		List<ISideBorder> sideBorders = SideBordersFactory.GetAllSideBorders();
 
-		public Border(Area area)
+		return this.isBorder(sideBorders, position);
+	}
+
+	public bool Is(List<ISideBorder> sideBorders, Position position)
+	{
+		return this.isBorder(sideBorders, position);
+	}
+
+	private bool isBorder(List<ISideBorder> sideBorders, Position position)
+	{
+		foreach (var sideBorder in sideBorders)
 		{
-			this.area = area;
+			if (sideBorder.Is(this.area, position))
+			{
+				return true;
+			}
 		}
 
-		public bool IsAt(Position position)
-		{
-			bool isUpper = this.isUpper(position);
-			bool isLower = this.isLower(position);
-			bool isLeft = this.isLeft(position);
-			bool isRight = this.isRight(position);
-
-			return (isUpper || isLower || isLeft || isRight);
-		}
-
-		private bool isUpper(Position position)
-		{
-			return position.X() == -1 ? true : false;
-		}
-
-		private bool isLower(Position position)
-		{
-			return position.X() == this.area.Height ? true : false;
-		}
-
-		private bool isLeft(Position position)
-		{
-			return position.Y() == -1 ? true : false;
-		}
-
-		private bool isRight(Position position)
-		{
-			return position.Y() == this.area.Width ? true : false;
-		}
+		return false;
 	}
 }
