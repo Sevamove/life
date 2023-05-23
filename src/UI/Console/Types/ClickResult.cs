fa287a6acb9cb0dffcc1f975e2dd00a8fdf772e3
Vsevolod <hello@vsevolod.nl>
2023-05-23 22:05:31 +0200

    fix: redefined button return type

---
+++ src/UI/Console/Types/ClickResult.cs
@@ -0,0 +1,11 @@
+using Infrastructure.DTO;
+using UI.Console.Enums;
+
+namespace UI.Console.Types;
+
+public record ClickResult()
+{
+	public required GameDTO[]? GameDTOs;
+	// Similar to the HTML href element's property.
+	public required Page? Page;
+}
