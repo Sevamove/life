122ad90332748b3d980ccf7f8c1be997bb840a2b
Vsevolod <hello@vsevolod.nl>
2023-05-25 14:09:24 +0200

    test: added LoadGame unit test

--- test/Application/Controllers/GameControllerTest.cs
+++ test/Application/Controllers/GameControllerTest.cs
@@ -20,10 +20,16 @@ public class GameControllerTest
 	[Fact]
 	public async void CreateNewGame_ReturnsGameDTO()
 	{
+		// Arrange.
+
 		IGame gameMock = new GameMock("123");
 		GameDTO gameDTO = GameDTOBuilder.GetGameDTO(gameMock);
 
+		// Act.
+
 		GameDTO expectedGame = await this.gameController.CreateNewGame(gameDTO);
+
+		// Assert.
 
 		Assert.True(expectedGame.Id == gameMock.Id());
 		Assert.True(expectedGame.Name == gameMock.Name());
@@ -48,9 +54,37 @@ public class GameControllerTest
 		Assert.True(expectedGames.Length > 0);
 	}
 
+	[Theory]
+	[InlineData("1", "My Game 1")]
+	[InlineData("5712&%$%&#   afl", "我的游戏")]
+	public async void LoadGame_ReturnsGameDTO(string actualGameId, string actualGameName)
+	{
+		// Arrange.
+
+		await this.clearTestDb();
+		await this.populateTestDbWithDummyData(actualGameId, actualGameName);
+
+		// Act.
+
+		GameDTO? expectedGame = await this.gameController.LoadGame(actualGameId);
+		string expectedGameId = expectedGame.Id;
+		string expectedGameName = expectedGame.Name;
+
+		// Assert.
+
+		Assert.True(expectedGame != null);
+		Assert.Equal(expectedGameId, actualGameId);
+		Assert.Equal(expectedGameName, actualGameName);
+	}
+
 	private async Task<bool> populateTestDbWithDummyData()
 	{
-		IGame gameMock = new GameMock("123", "My Game 123");
+		return await this.populateTestDbWithDummyData("123", "My Game 123");
+	}
+
+	private async Task<bool> populateTestDbWithDummyData(string gameId, string gameName)
+	{
+		IGame gameMock = new GameMock(gameId, gameName);
 		GameDTO gameDTO = GameDTOBuilder.GetGameDTO(gameMock);
 
 		GameDTO expectedGame = await this.gameController.CreateNewGame(gameDTO);
